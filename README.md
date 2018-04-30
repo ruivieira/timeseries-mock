588cae79082b28d07982ae19a1b97ae819bee586
Rui  Vieira <ruidevieira@googlemail.com>
2018-04-30 18:49:52 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # timeseries-mock
+
+# Acknowledgements
+
+This project is based on [elmiko](https://github.com/elmiko)'s 
+[Kafka OpenShift Python Emitter](https://github.com/bones-brigade/kafka-openshift-python-emitter), 
+a part of the [Bones Brigade](https://github.com/bones-brigade) set of OpenShift
+application skeletons.
---
+++ app.py
@@ -0,0 +1,65 @@
+import argparse
+import logging
+import os
+import time
+import urllib
+
+from kafka import KafkaProducer
+
+
+def main(args):
+    logging.info('brokers={}'.format(args.brokers))
+    logging.info('topic={}'.format(args.topic))
+    logging.info('rate={}'.format(args.rate))
+    logging.info('source={}'.format(args.source))
+
+    logging.info('downloading source')
+    dl = urllib.urlretrieve(args.source)
+    sourcefile = open(dl[0])
+
+    logging.info('creating kafka producer')
+    producer = KafkaProducer(bootstrap_servers=args.brokers)
+
+    logging.info('sending lines')
+    for line in sourcefile.readlines():
+        producer.send(args.topic, line)
+        time.sleep(1.0 / args.rate)
+    logging.info('finished sending source')
+
+
+def get_arg(env, default):
+    return os.getenv(env) if os.getenv(env, '') is not '' else default
+
+
+def parse_args(parser):
+    args = parser.parse_args()
+    args.brokers = get_arg('KAFKA_BROKERS', args.brokers)
+    args.topic = get_arg('KAFKA_TOPIC', args.topic)
+    args.rate = get_arg('RATE', args.rate)
+    args.source = get_arg('SOURCE_URI', args.source)
+    return args
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    logging.info('starting kafka-openshift-python emitter')
+    parser = argparse.ArgumentParser(description='emit some stuff on kafka')
+    parser.add_argument(
+        '--brokers',
+        help='The bootstrap servers, env variable KAFKA_BROKERS',
+        default='localhost:9092')
+    parser.add_argument(
+        '--topic',
+        help='Topic to publish to, env variable KAFKA_TOPIC',
+        default='bones-brigade')
+    parser.add_argument(
+        '--rate',
+        type=int,
+        help='Lines per second, env variable RATE',
+        default=3)
+    parser.add_argument(
+        '--source',
+        help='The source URI for data to emit, env variable SOURCE_URI')
+    args = parse_args(parser)
+    main(args)
+    logging.info('exiting')
