d7c9b9298e60ad1330cce3a73f806c2bf66f9c58
Rui  Vieira <ruidevieira@googlemail.com>
2018-05-30 11:22:06 +0100

    Add ARMA components

--- tests/parser_tests.py
+++ tests/parser_tests.py
@@ -190,3 +190,72 @@ observations:
 
         assert_equals(period, 0.5, "period has the wrong value")
         assert_equals(name, "Continuous Mean", "name has the wrong value")
+
+    def arma_default_test(self):
+        """Test if the default ARMA dimensions are correct
+        """
+        conf = """
+name: "ARMA"
+period: 0.5
+structure:
+  - type: arma
+    start: 1000.0
+    noise: 4.6
+observations:
+  type: continuous
+  noise: 5.5"""
+
+        d = _parse_string(conf)
+
+        model, state, period, name = parse_configuration(d)
+
+        assert_true(isinstance(model, NormalDLM), "model must be NormalDLM")
+        assert_true(isinstance(model.structure, UnivariateStructure),
+                    "structure must be UnivariateStructure")
+        assert_equals(model.structure.F.shape, (1, 1),
+                      "F has the wrong dimensions")
+        assert_equals(model.structure.G.shape, (1, 1),
+                      "G has the wrong dimensions")
+        assert_equals(model.structure.W.shape, (1, 1),
+                      "W has the wrong dimensions")
+
+        assert_equals(state.shape, (),
+                      "initial state has the wrong dimensions")
+
+        assert_equals(period, 0.5, "period has the wrong value")
+        assert_equals(name, "ARMA", "name has the wrong value")
+
+    def arma_test(self):
+        """Test if the ARMA(3) dimensions are correct
+        """
+        conf = """
+name: "ARMA(3)"
+period: 0.5
+structure:
+  - type: arma
+    start: 1000.0
+    coefficients: 0.1,0.3,0.5
+    noise: 4.6
+observations:
+  type: continuous
+  noise: 5.5"""
+
+        d = _parse_string(conf)
+
+        model, state, period, name = parse_configuration(d)
+
+        assert_true(isinstance(model, NormalDLM), "model must be NormalDLM")
+        assert_true(isinstance(model.structure, UnivariateStructure),
+                    "structure must be UnivariateStructure")
+        assert_equals(model.structure.F.shape, (3, 1),
+                      "F has the wrong dimensions")
+        assert_equals(model.structure.G.shape, (3, 3),
+                      "G has the wrong dimensions")
+        assert_equals(model.structure.W.shape, (3, 3),
+                      "W has the wrong dimensions")
+
+        assert_equals(state.shape, (3,),
+                      "initial state has the wrong dimensions")
+
+        assert_equals(period, 0.5, "period has the wrong value")
+        assert_equals(name, "ARMA(3)", "name has the wrong value")
